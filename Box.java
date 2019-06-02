43f92f7b3926a1d62ac586235e9b980ccea1ef9b
Hunter <huntercavers@xtra.co.nz>
2019-06-02 14:46:52 +1200

    Added new functionality to box, new mthod to set which face is on top, and new method to call values for top face

--- Box.java
+++ Box.java
@@ -6,12 +6,14 @@ class Box
 	private int x;
 	private int y;
 	private int z;
+	private int topFace;
 	
 	public Box(int d1, int d2, int d3)
 	{
 		x = d1;
 		y = d2;
 		z = d3;
+		topFace = 1;
 	}
 	
 	public int[] getFace1()
@@ -30,5 +32,37 @@ class Box
 	{
 		int[] array = new int[] {x, y, z};
 		return array;
+	}
+	
+	public void setTopFace(int face)
+	{
+		if(face < 1 || face > 3)
+		{
+			throw new IndexOutOfBoundsException("Index must be between 1 and 3");
+		}
+		else
+		{
+			topFace = face;
+		}
+	}
+	
+	public int [] getTopFace()
+	{
+		if(topFace == 1)
+		{
+			return getFace1();
+		}
+		else if(topFace == 2)
+		{
+			return getFace2();
+		}
+		else if(topFace == 3)
+		{
+			return getFace3();
+		}
+		else
+		{
+			return null;
+		}
 	}
 }

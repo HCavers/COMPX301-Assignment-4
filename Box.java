7ee881b2d741b78ec90e458cc6506283c7ccf2c4
Hunter <huntercavers@xtra.co.nz>
2019-06-08 11:40:36 +1200

    Documentation added to Box class

--- Box.java
+++ Box.java
@@ -3,11 +3,13 @@
 
 class Box
 {
-	private int x;
-	private int y;
-	private int z;
-	private int topFace;
+	// Instance Variables
+	private int x;			// x dimension of box
+	private int y;			// y dimension of box
+	private int z;			// z dimension of box
+	private int topFace;	// The top face of the box
 	
+	// Constructor
 	public Box(int d1, int d2, int d3)
 	{
 		x = d1;
@@ -16,44 +18,52 @@ class Box
 		topFace = 1;
 	}
 	
+	// Returns dimensions in face 1 orientation
 	public int[] getFace1()
 	{
 		int[] array = new int[] {x, y, z}; //0, 1, 2
 		return array;
 	}
 	
+	// Returns dimensions in face 2 orientation
 	public int[] getFace2()
 	{
 		int[] array = new int[] {x, z, y}; //0, 2, 1
 		return array;
 	}
 	
+	// Returns dimensions in face 3 orientation
 	public int[] getFace3()
 	{
 		int[] array = new int[] {y, x, z}; //1, 0, 2
 		return array;
 	}
 	
+	// Returns dimensions in face 4 orientation
 	public int[] getFace4()
 	{
 		int[] array = new int[] {y, z, x}; //1, 2, 0
 		return array;
 	}
 	
+	// Returns dimensions in face 5 orientation
 	public int[] getFace5()
 	{
 		int[] array = new int[] {z, x, y}; //2, 0, 1
 		return array;
 	}
 	
+	// Returns dimensions in face 6 orientation
 	public int[] getFace6()
 	{
 		int[] array = new int[] {z, y, x}; //2, 1, 0
 		return array;
 	}
 	
+	// Sets which face is on top
 	public void setTopFace(int face)
 	{
+		// Make sure inut is within bounds
 		if(face < 1 || face > 6)
 		{
 			throw new IndexOutOfBoundsException("Index must be between 1 and 6");
@@ -64,6 +74,7 @@ class Box
 		}
 	}
 	
+	// Returns dimensions in top face orientation
 	public int[] getTopFace()
 	{
 		if(topFace == 1)

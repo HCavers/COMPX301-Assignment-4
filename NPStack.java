ac6135709bccb2cc36f4f327674c71e2ea099ec5
Hunter <huntercavers@xtra.co.nz>
2019-06-08 11:36:53 +1200

    Added documentation to NPStack

--- NPStack.java
+++ NPStack.java
@@ -1,17 +1,23 @@
+// Hunter Cavers (1288108)
+// Brad Sissons (1300752)
+
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
 
 class NPStack
 {
-	static int temperature;
-	static int cooling;
+	// Default values of parameters
+	static int temperature = 10000;
+	static int cooling = 0.3;
 	static String inputFileName = "";
 	
 	public static void main(String[] args) throws FileNotFoundException {
+		// For each arguments
 		for(int i = 0; i < args.length; i++)
 		{
 			try
 			{
+				// Check if they match input parameters
 				if (args[i].equals("-f")) inputFileName = args[i + 1];
 				if (args[i].equals("-t")) temperature = Integer.parseInt(args[i + 1]);
 				if (args[i].equals("-c")) cooling = Integer.parseInt(args[i + 1]);
@@ -22,16 +28,20 @@ class NPStack
 			}
 		}
 		
+		// Check if file was entered
 		if(inputFileName == "")
 		{
 			System.out.println("Enter File Name:");
 			inputFileName = System.console().readLine();
 		}
 		
+		// Load in boxes from file
 		ArrayList<Box> boxes = loadBox.load(inputFileName);
+		// Make a random stack
 		BoxStack stack = RandomStack.getRS(boxes);
-		stack.print();
-		BoxStack newStack = Anneal.process(stack, 10000, .3, boxes);
+		// Anneal random for better solution
+		BoxStack newStack = Anneal.process(stack, temperature, cooling, boxes);
+		// Print stack
 		newStack.print();
 	}
 }
